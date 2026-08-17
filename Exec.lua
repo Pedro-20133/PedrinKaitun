c00f5559799c1e93ed558bb62bdd2a070a0841dd
Pedro-20133 <pedroHlq20133@gmail.com>
2026-08-17 19:54:46 -0300

    Update Exec.lua

--- Exec.lua
+++ Exec.lua
@@ -1,13 +1,15 @@
+repeat wait() until game:IsLoaded()
+
 _G.Configs = { -- Exemple
 	["Team"] = "Pirates",
-	
+
 	["Key"] = "KeyExemple", -- Optional
 	["Webhook"] = "WebhookExemple", -- Optional
-	
+
 	["LevelTarget"] = 2800,
 	["MoneyTarget"] = 100000,
 	["FragTarget"] = 1500,
-	
+
 	["GetCdk"] = true,
 	["GetTtk"] = false,
 	["GetGh"] = true,
