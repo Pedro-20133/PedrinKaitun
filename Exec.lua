fe4fce398a8651643bfe287ff0a4eae161d4ad8f
Pedro-20133 <pedroHlq20133@gmail.com>
2026-08-17 00:06:25 -0300

    Update and rename Exec.txt to Exec.lua

---
+++ Exec.lua
@@ -0,0 +1,6 @@
+_G.Configs = {
+  ["test1"] = "t1",
+  ["test2"] = "t2"
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Pedro-20133/PedrinKaitun/refs/heads/main/Files/Main.luau"))()
