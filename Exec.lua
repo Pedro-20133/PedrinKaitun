300523d9192a0460a837246cc049dce3967e0fc4
Pedro-20133 <pedroHlq20133@gmail.com>
2026-08-17 00:34:46 -0300

    Update Exec.lua

--- Exec.lua
+++ Exec.lua
@@ -1,6 +1,19 @@
-_G.Configs = {
-  ["test1"] = "t1",
-  ["test2"] = "t2"
+_G.Configs = { -- Exemple
+	["Team"] = "Pirates",
+	
+	["Key"] = "KeyExemple", -- Optional
+	["Webhook"] = "WebhookExemple", -- Optional
+	
+	["LevelTarget"] = 2800,
+	["MoneyTarget"] = 100000,
+	["FragTarget"] = 1500,
+	
+	["GetCdk"] = true,
+	["GetTtk"] = false,
+	["GetGh"] = true,
+	["GetHumanFullV4"] = true,
+	["GetMiticalFruit"] = true,
+	["GetSoulGuitar"] = true
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Pedro-20133/PedrinKaitun/refs/heads/main/Files/Main.luau"))()
