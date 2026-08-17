f5866c0fd62d67c7b5a1adf52184525235fb4da0
Pedro-20133 <pedroHlq20133@gmail.com>
2026-08-17 20:04:32 -0300

    Remove optional 'Key' from Configs
    Removed the optional 'Key' configuration from the global Configs table.

--- Exec.lua
+++ Exec.lua
@@ -3,7 +3,6 @@ repeat wait() until game:IsLoaded()
 _G.Configs = { -- Exemple
 	["Team"] = "Pirates",
 
-	["Key"] = "KeyExemple", -- Optional
 	["Webhook"] = "WebhookExemple", -- Optional
 
 	["LevelTarget"] = 2800,
